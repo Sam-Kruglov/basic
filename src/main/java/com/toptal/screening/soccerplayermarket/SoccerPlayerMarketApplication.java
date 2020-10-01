2d077b9a3b2d4b4c711504dfe67041455c90a120
Sam Kruglov <samkruglov@gmail.com>
2020-10-01 08:29:23 +0300

    initial commit
    spring.start.io

---
+++ src/main/java/com/toptal/screening/soccerplayermarket/SoccerPlayerMarketApplication.java
@@ -0,0 +1,13 @@
+package com.toptal.screening.soccerplayermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerPlayerMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerPlayerMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toptal/screening/soccerplayermarket/SoccerPlayerMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toptal.screening.soccerplayermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerPlayerMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
