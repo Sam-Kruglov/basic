1cd3d9189f0abf26376afc88db86adf9480c8aa3
Sam Kruglov <samkruglov@gmail.com>
2020-11-24 07:24:11 +0200

    add tests using api client

---
+++ soccer-player-market-backend/src/test/java/com/toptal/screening/soccerplayermarket/config/UserTestFactory.java
@@ -0,0 +1,36 @@
+package com.toptal.screening.soccerplayermarket.config;
+
+import com.toptal.screening.soccerplayermarket.domain.User;
+import com.toptal.screening.soccerplayermarket.repo.RoleRepo;
+import com.toptal.screening.soccerplayermarket.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
+public class UserTestFactory {
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    public User createAdmin(String email, String password) {
+        return create(email, password, Roles.USER, Roles.ADMIN);
+    }
+
+    public User createUser(String email, String password) {
+        return create(email, password, Roles.USER);
+    }
+
+    public User create(String email, String password, String... roleNames) {
+        val roles = Stream.of(roleNames).map(roleRepo::findByName).collect(toList());
+        val user = new User(email, passwordEncoder.encode(password), roles);
+        userRepo.save(user);
+        return user;
+    }
+}
---
+++ soccer-player-market-backend/src/test/resources/clear_db.sql
@@ -0,0 +1,5 @@
+delete
+from users_to_roles;
+delete
+from users;
+-- keep roles, they are kind of part of the schema
