a1e815f21a6ed380bdba77ee234f9a7d4c578fe2
Sam Kruglov <samkruglov@gmail.com>
2020-12-12 17:03:59 +0200

    remove redundant annotations

--- base-backend/src/main/java/com/samkruglov/base/api/view/request/CreateUserDto.java
+++ base-backend/src/main/java/com/samkruglov/base/api/view/request/CreateUserDto.java
@@ -1,7 +1,6 @@
 package com.samkruglov.base.api.view.request;
 
 import com.samkruglov.base.api.view.constraint.UserFirstOrSecondName;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 import javax.validation.constraints.Email;
@@ -10,12 +9,8 @@ import javax.validation.constraints.NotNull;
 
 @Value
 public class CreateUserDto {
-    @Schema(required = true)
     @NotNull @UserFirstOrSecondName String firstName;
-
-    @Schema(required = true)
     @NotNull @UserFirstOrSecondName String lastName;
-
     @NotBlank @Email String email;
     @NotBlank        String password;
 }
